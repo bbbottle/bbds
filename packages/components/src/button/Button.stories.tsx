7e1651a2ed21e79241b11e35745316a7715a6d95
bbbottle <z@zjh.im>
2021-12-21 22:24:09 +0800

    refactor(stories): add basic controls

--- packages/components/src/button/Button.stories.tsx
+++ packages/components/src/button/Button.stories.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
-import { Button, ButtonType } from "./Button";
+import { Button, ButtonProps, ButtonType } from "./Button";
 
 export default {
   title: "Button",
   component: Button,
+  argTypes: {
+    type: {
+      options: [ButtonType.PRIMARY, ButtonType.DISABLED, ButtonType.NORMAL, ButtonType.DANGER],
+      defaultValue: ButtonType.PRIMARY,
+      control: { type: 'select' }, // Automatically inferred when 'options' is defined
+    },
+  },
 };
 
 const onClick = console.log;
 
-export const Default = () => <Button type={ButtonType.PRIMARY} onClick={onClick}>Button</Button>;
-export const Danger = () => <Button type={ButtonType.DANGER} onClick={onClick}>Button</Button>;
-export const Normal = () => <Button type={ButtonType.NORMAL} onClick={onClick}>Button</Button>;
-export const Disabled = () => <Button type={ButtonType.DISABLED} onClick={onClick}>Button</Button>;
+export const Default = (props: ButtonProps) => <Button type={ props.type || ButtonType.PRIMARY} onClick={onClick}>Button</Button>;
--- packages/components/src/link/Link.stories.tsx
+++ packages/components/src/link/Link.stories.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 
-import { Link, LinkColor } from "./Link";
+import { Link, LinkColor, LinkProps } from "./Link";
 import { HashRouter as Router } from "react-router-dom";
 
 export default {
   title: 'Link',
   component: Link,
+  argTypes: {
+    color: {
+      options: [LinkColor.BLUE, LinkColor.RED, LinkColor.GRAY],
+      control: { type: 'select' }, // Automatically inferred when 'options' is defined
+      defaultValue: LinkColor.BLUE,
+    },
+    external: {
+      type: 'boolean',
+      defaultValue: false,
+      control: { type: 'boolean' },
+    },
+    to: {
+      defaultValue: "/foo/bar",
+    }
+  }
 };
 
-export const Default = () => <Router><Link to="/foo/bar">Foo</Link></Router>
-export const External = () => <Router><Link to="https://bbki.ng" external>bbki.ng</Link></Router>
-export const Gray = () => <Router><Link to="https://bbki.ng" external color={LinkColor.GRAY}>bbki.ng</Link></Router>
+export const Default = (props: LinkProps) => (
+  <Router>
+    <Link
+      to={props.external ? 'https://bbki.ng' : '/foo/bar'}
+      external={props.external}
+      color={props.color}
+    >
+      Foo
+    </Link>
+  </Router>
+)
