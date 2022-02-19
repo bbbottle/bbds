e0d44e189806f80cb2e6af6d19c3c13a0300ab75
bbbottle <z@zjh.im>
2022-02-20 00:43:41 +0800

    feat(component): add skeleton component

---
+++ packages/components/src/skeleton/Page.stories.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Page } from "./Page";
+import { Nav } from "../nav/Nav";
+import { HashRouter as Router } from "react-router-dom";
+import { Link, LinkColor } from "../link/Link";
+
+const paths = [
+  {
+    name: "~",
+    path: "/",
+  },
+  {
+    name: "ext",
+    path: "/ext",
+  },
+  {
+    name: "txt",
+  },
+];
+
+export default {
+  title: "Page",
+  component: Page,
+};
+
+export const Default = () => (
+  <Router>
+    <Page
+      nav={<Nav paths={paths} />}
+      main={<div>hello</div>}
+      footer={
+        <Link to="/world" color={LinkColor.GRAY}>
+          hello
+        </Link>
+      }
+    />
+  </Router>
+);
