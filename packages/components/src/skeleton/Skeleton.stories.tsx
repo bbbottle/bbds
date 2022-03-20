6ae44cfaccbfc54a70cc3a79cd3e2ba62d534cf7
bbbottle <z@zjh.im>
2022-03-20 14:06:51 +0800

    feat: add skeleton

--- packages/components/src/skeleton/Skeleton.stories.tsx
+++ packages/components/src/skeleton/Skeleton.stories.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
-import { SkeletonColor, Skeleton, SkeletonProps } from "./Seleton";
+import {
+  Skeleton,
+  SkeletonColor,
+  SkeletonProps,
+  ArticleSkeleton,
+  LinkListSkeleton,
+} from "./Seleton";
 
 export default {
   title: "Skeleton",
   component: Skeleton,
   argTypes: {
     bgColor: {
-      options: [SkeletonColor.RED, SkeletonColor.BLUE, SkeletonColor.GRAY],
+      options: [
+        SkeletonColor.RED,
+        SkeletonColor.BLUE,
+        SkeletonColor.GRAY,
+        SkeletonColor.BLACK,
+      ],
       defaultValue: SkeletonColor.BLUE,
     },
     width: {
@@ -20,3 +31,15 @@ export default {
 };
 
 export const Default = (props: SkeletonProps) => <Skeleton {...props} />;
+
+export const SkeletonForArticle = () => {
+  return (
+    <ArticleSkeleton titleLength={2}>
+      <Skeleton width={100} />
+    </ArticleSkeleton>
+  );
+};
+
+export const SkeletonForLinkList = () => {
+  return <LinkListSkeleton titleLength={2} linksLength={[3, 4, 2, 5, 6]} />;
+};
