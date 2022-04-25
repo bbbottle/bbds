4b6c4644e526ec6d0ec2aec6d638321ce796a2c8
bbbottle <z@zjh.im>
2022-04-25 19:10:55 +0800

    fix: remove hr element

--- packages/components/src/article/Article.tsx
+++ packages/components/src/article/Article.tsx
@@ -14,8 +14,7 @@ export const Article: FunctionComponent<ArticleProps> = (props) => {
   const { title, content, children, className, description } = props;
   return (
     <div className={classNames("article", className)}>
-      <div className="text-[2.25rem] leading-none">{title}</div>
-      <hr className="my-64 border-gray-200" />
+      <div className="text-[2.25rem] mb-128 leading-none">{title}</div>
       {description && <div className="mb-128">{description}</div>}
       <div className="text-gray-700">{children || content}</div>
     </div>
