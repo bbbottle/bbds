f212169e09689b3c65efee821a6d1ac18c32de88
bbbottle <z@zjh.im>
2022-05-08 14:57:44 +0800

    feat(components/spinner): add new compoennt

---
+++ packages/components/src/spinner/Spinner.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Spinner, SpinnerProps } from "./Spinner";
+
+export default {
+  title: "Spinner",
+  component: Spinner,
+  argTypes: {
+    width: {
+      defaultValue: 2,
+    },
+    color: {
+      defaultValue: "#D1D5DB",
+    },
+  },
+};
+
+export const Default = (props: SpinnerProps) => (
+  <Spinner
+    {...props}
+    pathClassName="stroke-gray-200"
+    className="animate-spin"
+  />
+);
